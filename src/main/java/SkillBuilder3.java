d35332021a29e8612978033516cfff647175d7b2
Thomas Pendleton III <105595323+Tpenny68@users.noreply.github.com>
2025-02-13 12:20:33 -0500

    Submission

--- src/main/java/SkillBuilder3.java
+++ src/main/java/SkillBuilder3.java
@@ -11,43 +11,64 @@ public class SkillBuilder3
     private static String repeat(String s, int numOfTimesToRepeat)
     {
         String result = "";
-        // TODO: replace this line with your code
+        for(int i = 0; i < numOfTimesToRepeat; i++){
+            result += s;
+        }
         return result;
     }
     public static String leftRightTriangle(int height)
     {
         String str = "";
-        // TODO: replace this line with your code
+        for (int i = 1; i <= height; i++){
+            str += repeat("*",i) + "\n";
+        }
         return str;
     }
 
     public static String rightRightTriangle(int height)
     {
         String str = "";
-        // TODO: replace this line with your code
+        for (int i = 1; i <= height; i++){
+            str += repeat(" ",height - i) + repeat("*",i) + "\n";
+        }
         return str;
     }
 
     public static String circle(int radius)
     {
         String str = "";
-        // TODO: replace this line with your code
+        for (int i = radius - 1; i > -radius; i--){
+            int limit = (i != -radius) ? (int) Math.floor(Math.sqrt(radius * radius - i * i)) : radius;
+            str += repeat(" ",radius - limit) + repeat("*",limit) + repeat("*",limit) + "\n";
+        }
         return str;
     }
 
     public static long sumOfDivisors(long number) {
         long sum = 0;
-        // TODO: replace this line with your code
+        for(int i = 1; i < number; i++){
+            if (number % i == 0){
+                sum += i;
+            }
+        }
         return sum;
     }
 
     public static boolean isPerfect(long number){
-        return false;
+        if (sumOfDivisors(number) == number){
+            return true;
+        } else return false;
     }
 
-    public static long sumOfDivisorsUsingWhile(long number){
+    public static long sumOfDivisorsUsingWhile(long number) {
         long sum = 0;
-        // TODO: replace this line with your code
+        int i = 1;
+        while(i < number){
+            if (number % i == 0){
+                sum += i;
+            }
+            i += 1;
+        }
         return sum;
     }
 }
